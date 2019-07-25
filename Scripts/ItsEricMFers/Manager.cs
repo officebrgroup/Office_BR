95947995ce7eee5c6575de647990a57d6f894ff3
Ericrack <errrrrricg@gmail.com>
2019-07-25 13:02:27 -0700

    Add files via upload

---
+++ Scripts/ItsEricMFers/Manager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject enemyPrefab; //the prefab of the enemy
+    public int numEnemy = 10; //number of enemies
+    public GameObject[] allEnemy; //array of enemies
+    public Vector3 spaceLimit = new Vector3(50, 50, 50);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allEnemy = new GameObject[numEnemy];
+        for (int i=0; i<numEnemy; i++)
+        {
+            Vector3 pos = this.transform.position +
+                new Vector3(Random.Range(-spaceLimit.x, spaceLimit.x),
+                0,
+                Random.Range(-spaceLimit.z, spaceLimit.z));
+            allEnemy[i] = (GameObject)Instantiate(enemyPrefab, pos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/ItsEricMFers/bad_guy_follow.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+
+
+public class bad_guy_follow : MonoBehaviour
+{
+    public GameObject good_g;
+
+    //All Variables
+    public float detectDistance = 10.0f; //within this distance, the good guy is detected
+    public float closeEnough = 1.0f; //within this distance, object stop moving
+    public float frequency = 0.2f; //frequency in percent that changes random position
+
+    public float areaX = 50.0f; //x length of the ground
+    public float areaZ = 50.0f; //z length of the ground
+    public Vector3 starting_pos = Vector3.zero; //starting position of this stuff
+
+    public bool detected = false; //decected the object or not
+
+
+    public NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent.SetDestination(new Vector3(Random.Range(-50.0f, 50.0f), 0, Random.Range(-50.0f, 50.0f)));
+        starting_pos = new Vector3(Random.Range(-50.0f, 50.0f), 0, Random.Range(-50.0f, 50.0f));
+        this.transform.position = starting_pos;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position, good_g.transform.position) <= detectDistance)
+        { 
+            follow();
+            detected = true;
+        }
+
+        else
+        {
+            randomGo();
+            detected = false;
+        }
+
+
+    }
+
+    void follow()
+    {
+        agent.SetDestination(good_g.transform.position);
+    }
+
+    void randomGo()
+    {
+        if (Random.Range(0.0f, 100.0f) <= frequency)
+            agent.SetDestination(new Vector3(Random.Range(-50.0f, 50.0f), 0, Random.Range(-50.0f, 50.0f)));
+    }
+}
